d6c8c2c9dd43e8e635d4ce2230a9edb0caaddcaa
HoganBerens <berenshogan@gmail.com>
2023-07-22 11:36:44 -0500

    add oauth

---
+++ models/scores.js
@@ -0,0 +1,9 @@
+/* user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  userName: String,
+  userAvatar: String
+}, */
+//this is how you reference the user's objectId that created the score
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const ensureLoggedIn = require("../config/ensureLoggedIn");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* router.get("/", moviesCtrl.index);
+// Use ensureLoggedIn middleware to protect routes
+router.get("/new", ensureLoggedIn, moviesCtrl.new);
+router.get("/:id", moviesCtrl.show);
+router.post("/", ensureLoggedIn, moviesCtrl.create); */
 
 module.exports = router;
