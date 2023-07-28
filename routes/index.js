b443416d709de2fd2db9f7de8bb5c82431b1b5a7
HoganBerens <berenshogan@gmail.com>
2023-07-28 09:07:21 -0500

    fix dashboard bug

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,9 @@ router.get("/", function (req, res, next) {
 
 router.get(
   "/auth/google",
-  passport.authenticate(
-    // Which passport strategy is being used?
-    "google",
-    {
-      // Requesting the user's profile and email
-      scope: ["profile", "email"],
-      // Optionally force pick account every time
-      // prompt: "select_account"
-    }
-  )
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
 );
 router.get(
   "/oauth2callback",
